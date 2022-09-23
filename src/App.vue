9729780a72183926d81f3c194676cb9c603b4d29
LuisEnrique <46499209+LuisEML@users.noreply.github.com>
2022-09-22 19:11:34 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@
         <div class="card">
           <img src="./img/2.jpg" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Carne</h5>
+            <h5 class="card-title">Carné</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
               content. This content is a little bit longer.</p>
             </div>
@@ -100,7 +100,7 @@
     <div class="card">
       <img src="./img/2.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Carne</h5>
+        <h5 class="card-title">Carné</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
           content. This content is a little bit longer.</p>
         </div>
